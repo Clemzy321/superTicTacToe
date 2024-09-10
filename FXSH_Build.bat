1accb0dc2b21d703d45af3ff9f69cea0fce6d429
Clemzy321 <clemzy321@gmail.com>
2024-09-10 19:25:26 +0200

    Kreis gemacht

---
+++ FXSH_Build.bat
@@ -0,0 +1,22 @@
+@echo off
+rem Do not edit! This batch file is created by CASIO fx-9860G SDK.
+
+
+if exist STTT.G1A  del STTT.G1A
+
+cd debug
+if exist FXADDINror.bin  del FXADDINror.bin
+"D:\Program Files\Casio\OS\SH\Bin\Hmake.exe" Addin.mak
+cd ..
+if not exist debug\FXADDINror.bin  goto error
+
+"D:\Program Files\Casio\Tools\MakeAddinHeader363.exe" "C:\Users\Nutzer\OneDrive\Dokumente\GitHub\superTicTacToe"
+if not exist STTT.G1A  goto error
+echo Build has completed.
+goto end
+
+:error
+echo Build was not successful.
+
+:end
+
